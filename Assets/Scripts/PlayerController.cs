d37677a0d91fea8d0d97891c3a4609c061c7f8e8
Zach Tucker <ZachTucker385@gmail.com>
2018-07-17 15:47:44 -0700

    initial commit, basic player movement, start on game manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float radius;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("happened");
+        var thisCollider = GetComponent<Collider>();
+
+        if (!thisCollider)
+        {
+            return;
+        }
+
+            var collider = collision.collider;
+
+            Vector3 otherPosition = collider.gameObject.transform.position;
+            Quaternion otherRotation = collider.gameObject.transform.rotation;
+
+            Vector3 direction;
+            float distance;
+
+            bool overlapped = Physics.ComputePenetration(thisCollider, transform.position, transform.rotation, collider, otherPosition, otherRotation, out direction, out distance);
+
+            if (overlapped)
+            {
+            transform.position += (direction * distance);
+            }
+        }
+    }
---
+++ Assets/Scripts/ShowPenetration.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPenetration : MonoBehaviour {
+
+    public float radius = 3f;
+    public int maxNeighbors = 16;
+
+    private Collider[] neighbors;
+
+    public void Start()
+    {
+        neighbors = new Collider[maxNeighbors];
+    }
+
+    public void OnDrawGizmos()
+    {
+        var thisCollider = GetComponent<Collider>();
+
+        if (!thisCollider)
+        {
+            return;
+        }
+
+        int count = Physics.OverlapSphereNonAlloc(transform.position, radius, neighbors);
+
+        for (int i = 0; i < count; ++i)
+        {
+            var collider = neighbors[i];
+
+            if (collider == thisCollider)
+            {
+                continue;
+            }
+
+            Vector3 otherPosition = collider.gameObject.transform.position;
+            Quaternion otherRotation = collider.gameObject.transform.rotation;
+
+            Vector3 direction;
+            float distance;
+
+            bool overlapped = Physics.ComputePenetration(thisCollider, transform.position, transform.rotation, collider, otherPosition, otherRotation, out direction, out distance);
+
+            if (overlapped)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(otherPosition, -direction * distance);
+            }
+        }
+    }
+}
